dc8e79994643b47880559d29f727a8cece40409e
ADMIN\admin <Ihtyyarovich@gmail.com>
2024-01-08 07:53:54 +0300

    fixed navbar style and rules page added into routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RulesComponent } from './rules/rules.component';
 
-const routes: Routes = [{ path: 'game', loadChildren: () => import('./game/game.module').then(m => m.GameModule) }];
+const routes: Routes = [
+  { path: 'rules', component: RulesComponent },
+  { path: 'game', loadChildren: () => import('./game/game.module').then(m => m.GameModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
